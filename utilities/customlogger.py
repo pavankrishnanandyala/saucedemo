09a4543e821d07636128e34f704487d778a53e24
pavankrishnanandyala <pavankrishna.nandyala@gmail.com>
2025-02-14 19:40:21 +0530

    update v3

--- utilities/customlogger.py
+++ utilities/customlogger.py
@@ -1,10 +1,21 @@
 import logging
+import os
 
-class logGen():
+
+class logGen:
     @staticmethod
     def logger():
-        logging.basicConfig(filename="..\\Logs\\automation.log",
-                            format="%(asctime)s: %(levelname)s: %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p", level=logging.DEBUG, force=True)
-        logger = logging.getLogger()
-        logger.setLevel(logging.INFO)
-        return logger
+        # Create Logs directory if it doesn't exist
+        log_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Logs")
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        # Set up logging with absolute path
+        log_file = os.path.join(log_dir, "automation.log")
+        logging.basicConfig(
+            filename=log_file,
+            format='%(asctime)s: %(levelname)s: %(message)s',
+            datefmt='%m/%d/%Y %I:%M:%S %p',
+            level=logging.INFO
+        )
+        return logging.getLogger()
