e765f8a66113dc204fa30db8457f42cc9b81abee
pavankrishnanandyala <pavankrishna.nandyala@gmail.com>
2025-02-14 19:44:11 +0530

    v4

--- testcases/test_login.py
+++ testcases/test_login.py
@@ -3,47 +3,41 @@ from utilities.customlogger import logGen
 from utilities.ReadProperties import ReadConfig
 import pytest
 
-
-class test_001_Login():
-
-
-    baseUrl = ReadConfig.getbaseUrl()
+class Test001Login:
+    baseUrl = ReadConfig.GetAplicationUrl()
     username = ReadConfig.getusername()
     password = ReadConfig.getpassword()
     logger = logGen.logger()
     expected_url = "https://www.saucedemo.com/inventory.html"
 
-
-
     def test_pagetitle(self, setup):
-       self.logger.info("*** Started Page Title Test ***")
-       self.driver = setup
-       self.logger.info("*** Opening the url ***")
-       self.driver.get(self.baseUrl)
+        self.logger.info("*** Started Page Title Test ***")
+        self.driver = setup
+        self.logger.info("*** Opening the url ***")
+        self.driver.get(self.baseUrl)
 
-       if self.driver.title == "Swag Labs":
-          self.logger.info("*** Page Title Test Passed ***")
-          self.driver.close()
-          assert True
-       else:
-           self.logger.error("*** Page Title Test Failed")
-           self.driver.close()
-           assert False
-
+        if self.driver.title == "Swag Labs":
+            self.logger.info("*** Page Title Test Passed ***")
+            self.driver.close()
+            assert True
+        else:
+            self.logger.error("*** Page Title Test Failed ***")
+            self.driver.close()
+            assert False
 
     def test_login(self, setup):
-         self.logger.info("*** Started Login Test ***")
-         self.driver = setup
-         self.driver.get(self.baseUrl)
-         self.lp = LoginPage(self.driver)
-         self.lp.setUserName(self.username)
-         self.lp.setPasssword(self.password)
-         self.lp.clicklogin()
-         if self.driver.current_url == self.expected_url:
+        self.logger.info("*** Started Login Test ***")
+        self.driver = setup
+        self.driver.get(self.baseUrl)
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPasssword(self.password)
+        self.lp.clicklogin()
+        if self.driver.current_url == self.expected_url:
             self.logger.info("*** Login Test Passed ***")
             self.driver.close()
             assert True
-         else:
+        else:
             self.logger.error("*** Login Test Failed ***")
             self.driver.save_screenshot("..\\Screenshots\\" + "test_Login.png")
             self.driver.close()
--- utilities/customlogger.py
+++ utilities/customlogger.py
@@ -1,21 +1,15 @@
 import logging
-import os
-
 
 class logGen:
     @staticmethod
     def logger():
-        # Create Logs directory if it doesn't exist
-        log_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Logs")
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-
-        # Set up logging with absolute path
-        log_file = os.path.join(log_dir, "automation.log")
         logging.basicConfig(
-            filename=log_file,
-            format='%(asctime)s: %(levelname)s: %(message)s',
-            datefmt='%m/%d/%Y %I:%M:%S %p',
-            level=logging.INFO
+            filename="..\\Logs\\automation.log",
+            format="%(asctime)s: %(levelname)s: %(message)s",
+            datefmt="%m/%d/%Y %I:%M:%S %p",
+            level=logging.DEBUG,
+            force=True
         )
-        return logging.getLogger()
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        return logger
